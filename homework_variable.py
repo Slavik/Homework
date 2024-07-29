33c48443f53634bb6023e7d4376f625ddf6c3681
Slavik <106432639+Slavik@users.noreply.github.com>
2024-07-29 22:54:57 +0500

    Add files via upload

---
+++ homework_variable.py
@@ -0,0 +1,5 @@
+quaComHome = 12
+quaSpentHours = 1.5
+nameOfCurs = "Python"
+timeToOneCom = quaSpentHours / quaComHome
+print("Курс: " + str(nameOfCurs) + ", всего задач: " + str(quaComHome) + ", затрачено часов: " + str(quaSpentHours) + ", среднее время выполнения " + str(timeToOneCom) +  " часа.")
